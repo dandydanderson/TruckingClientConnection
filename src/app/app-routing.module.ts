aff3ebcdb828dde4c8f74e4c1ac661b1f311a979
Your Name <you@example.com>
2020-03-19 17:12:35 -0500

    added route for trucking and 404 page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TruckRegistrationComponent } from './components/truck-registration/truck-registration.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  {path: 'register/carrier', component: TruckRegistrationComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
