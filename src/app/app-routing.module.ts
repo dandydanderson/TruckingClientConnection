ba5fc3777e5ddbf7651398885b9d76ec66d671d0
Your Name <you@example.com>
2020-03-20 16:12:52 -0500

    added freight-dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { TruckRegistrationComponent } from './components/truck-registration/truck-registration.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ClientRegistrationComponent } from './components/client-registration/client-registration.component';
+import { FreightDashboardComponent } from './freight-dashboard/freight-dashboard.component';
 
 
 
 const routes: Routes = [
   
   {path: 'register/carrier', component: TruckRegistrationComponent},
+  {path: 'freight-dashboard', component: FreightDashboardComponent},
   {path: 'register/client', component: ClientRegistrationComponent},
   {path: '**', component: NotFoundComponent}
 ];
