e9b5c662e9efaadbcd2e10378286ce5cef02fae9
Your Name <you@example.com>
2020-03-21 23:12:40 -0500

    fixed the app.module.ts repeated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClientComponent } from './components/client/client.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
 import { OrdersComponent } from './components/orders/orders.component';
-
-// './components/client/client.component'
 import { FreightDashboardComponent } from './freight-dashboard/freight-dashboard.component';
 import { RoutesComponent } from './routes/routes.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -32,14 +25,14 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { TruckRegistrationEquipmentComponent } from './components/truck-registration-equipment/truck-registration-equipment.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
-    OrdersComponent
+    OrdersComponent,
     FreightDashboardComponent,
     RoutesComponent,
     TruckRegistrationComponent,
